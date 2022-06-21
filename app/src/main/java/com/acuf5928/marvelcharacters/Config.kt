a933536935b2ee39653f633fc4e1b63c39af39b6
Alberto Antonino Cuffaro <alberto.cuffaro@hotmail.it>
2022-06-21 21:00:03 +0200

    Add config

---
+++ app/src/main/java/com/acuf5928/marvelcharacters/Config.kt
@@ -0,0 +1,7 @@
+package com.acuf5928.marvelcharacters
+
+data class Config(
+    val baseUrl: String = "https://gateway.marvel.com/v1/public/",
+    val publicKey: String = "PUT_YOU_KEY_HEHE",
+    val privateKey: String = "PUT_YOU_KEY_HEHE"
+)
